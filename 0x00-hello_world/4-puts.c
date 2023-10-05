ab2ad85fdc5c3c101f0d1df49711f0422c8d74d2
wisdomncube <wizzsiyancube@icloud.com>
2023-10-05 10:37:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-*main - this is the main put function
-*
-*return:0
-*/
+ * main - Entry point for the program
+ *
+ * Return: 0 to indicate success
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
