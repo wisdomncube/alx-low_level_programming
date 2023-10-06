dd1a13bb93793f982fb02229a5514c81233659aa
wisdomncube <wizzsiyancube@icloud.com>
2023-10-06 08:59:17 -0700

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,6 +9,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -16,6 +17,6 @@ int main(void)
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
