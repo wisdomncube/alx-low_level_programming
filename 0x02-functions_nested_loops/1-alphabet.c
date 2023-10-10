bdae44ea2811c5c1fbe7fa82193bc26a82a5d79b
wisdomncube <wizzsiyancube@icloud.com>
2023-10-10 11:38:07 -0700

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - function
+ * print_alphabet - function
  * Return: 0
  */
-
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+    char letter = 'a';
 
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+    putchar('\n');
 }
