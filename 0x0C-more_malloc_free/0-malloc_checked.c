1096aff6fd2fb8eba183df7c120e095ca40128eb
wisdomncube <wizzsiyancube@icloud.com>
2023-11-02 11:48:27 -0700

    malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,22 +1,17 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-* malloc_checked - allocates memory
-* @b: the size to allcoate
-*
-* Return: nothing
-*/
-
+ * malloc_checked - Main Entry
+ * @b: input
+ * Return: 0
+ */
 void *malloc_checked(unsigned int b)
 {
-void *p;
+	void *p;
 
-p = malloc(b);
-
-if (p == NULL)
-exit(98);
-
-return (p);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
