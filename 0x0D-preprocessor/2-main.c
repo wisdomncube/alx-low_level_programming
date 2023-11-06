b8fdad15923c05c2565e15c777f9857ea6863646
wisdomncube <wizzsiyancube@icloud.com>
2023-11-06 09:01:09 -0800

    main

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main() {
-printf("Compiled from file: %s\n", __FILE__);
-return 0;
+/**
+ * main - Entry point of the program
+ *
+ * Description - a program that prints where the file has been compiled from
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+return (0);
 }
