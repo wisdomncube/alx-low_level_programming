5f2236fd310e6f7528b2d781cc719bb8cfebeee0
wisdomncube <wizzsiyancube@icloud.com>
2023-11-06 07:56:35 -0800

    main

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+* main - compilation
+* description - a program that the name of the file it was compiled from
+* return: 0
+*/
+
 int main(void) 
 {
+
 printf("%s\n", __FILE__);
+
 return (0);
 }
