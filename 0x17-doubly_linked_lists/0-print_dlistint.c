c945a6b2c148d77646fa8ec2027f6b6cd3af05d3
wisdomncube <wizzsiyancube@icloud.com>
2024-01-11 04:21:35 -0800

    refactoring code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,16 +9,16 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    const dlistint_t *current = h;
-    size_t count = 0;
+const dlistint_t *current = h;
+size_t count = 0;
 
-    while (current != NULL)
-    {
-        printf("%d\n", current->n);
-        count++;
-        current = current->next;
-    }
+while (current != NULL)
+{
+printf("%d\n", current->n);
+count++;
+current = current->next;
+}
 
-    return count;
+return count;
 }
 
