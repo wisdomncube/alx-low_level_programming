1803bb561faa19d9421495100cc298b1576d4417
wisdomncube <wizzsiyancube@icloud.com>
2024-01-11 04:24:59 -0800

    refacoring code

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
@@ -8,11 +9,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    size_t count = 0;
-    while (h != NULL)
-    {
-        count++;
-        h = h->next;
-    }
-    return count;
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return count;
 }
